3ed7d9f8667987c56d0202973a3614b5ab8e216e
Ivan <sr.ivanguirado@gmail.com>
2024-05-03 17:48:13 +0200

    primer commit

---
+++ src/tarea6ed/Tarea6EDClase.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea6ed;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Tarea6EDClase {
+
+    public final double DESCUENTO1 = 0.8;
+    public final double REDUCCION = 5;
+    public final int NUMERO_PRODUCTOS = 3;
+    public final double DESCUENTO2 = 0.95;
+
+    public void aplicarDescuento(double precioProducto, int numProductos) {
+        double total;
+        if (numProductos > NUMERO_PRODUCTOS) {
+            precioProducto -= REDUCCION;
+        }
+        if (numProductos != 0) {
+            total = precioProducto * DESCUENTO1;
+            mostrarTotal(total);
+        } else {
+            total = precioProducto * DESCUENTO2;
+            mostrarTotal(total);
+        }
+
+    }
+
+    public void mostrarTotal(double total) {
+
+        System.out.println("El total a pagar es:" + total);
+        System.out.println("Enviado");
+    }
+}
