f458d862563789f5c5805d22614cd863dd14063c
Ivan <sr.ivanguirado@gmail.com>
2024-05-03 18:02:08 +0200

    segundo commit

---
+++ src/tarea6ed/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea6ed;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+        miCarrito.aplicarDescuento(100, 5);
+
+    }
+
+}
