b1f76673e7e8231736428f538722b968a7160eb6
Ivan <sr.ivanguirado@gmail.com>
2024-05-10 19:54:02 +0200

    tercer commit

--- src/tarea6ed/Tarea6EDClase.java
+++ src/tarea6ed/Tarea6EDClase.java
@@ -9,7 +9,11 @@ package tarea6ed;
  * @author Ivan
  */
 public class Tarea6EDClase {
-
+    
+    // Constantes de clase para definir el primer descuento aplicable, 
+    //la reducción de precio,
+    // el número de productos a partir del cual se aplica la reducción, 
+    //y el segundo descuento.
     public final double DESCUENTO1 = 0.8;
     public final double REDUCCION = 5;
     public final int NUMERO_PRODUCTOS = 3;
@@ -17,23 +21,28 @@ public class Tarea6EDClase {
 
     public void aplicarDescuento(double precioProducto, int numProductos) {
         double total;
+        // Si la cantidad de productos supera el número establecido, 
+        //se aplica una reducción al precio.
         if (numProductos > NUMERO_PRODUCTOS) {
             precioProducto -= REDUCCION;
         }
+        // Si hay al menos un producto, se aplica un descuento del 20% al
+        //precio después de cualquier reducción.
         if (numProductos != 0) {
             total = precioProducto * DESCUENTO1;
             mostrarTotal(total);
         } else {
+            // Si no hay productos, se aplica un descuento del 5% al precio 
+            //original.
             total = precioProducto * DESCUENTO2;
             mostrarTotal(total);
         }
 
     }
     
-
+ // Método para mostrar el total a pagar en la consola.
     public void mostrarTotal(double total) {
         System.out.println("El total a pagar es:" + total);
         System.out.println("Enviado");
     }
 }
-
